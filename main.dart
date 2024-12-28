f46560e8f398f8ad86a1e9ab3b6bf1585c4bc3f8
moustafamaher13 <35843188+moustafamaher13@users.noreply.github.com>
2024-12-28 14:51:05 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,16 @@
 void main() {
+  //example of the list
   List numbers = [5,10,100,50, -100];
   
  int finalNumber = minimum(numbers);
   
   print("The Minimum Number is $finalNumber");
 }
-
+//MinimumValue Function
 int minimum(List numbers){
    
   int minimumNumber = numbers[0];
-  
+  //THe loop and if condition to get the minimun number
   for(int number in numbers){
     if(number < minimumNumber){
       minimumNumber = number;
